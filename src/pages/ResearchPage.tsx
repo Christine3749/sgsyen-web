5508f4b1f8eb1c58270f5e001f59c61072d92e8c
Ethan Li <Ethan7586@gsyen.com>
2026-06-21 03:05:04 +0800

    Restore research page pearl layout

--- src/pages/ResearchPage.tsx
+++ src/pages/ResearchPage.tsx
@@ -10,8 +10,6 @@ import { research, Article, PolicyEvent } from '../lib/research';
 import { supabase } from '../lib/supabase';
 import { useLocale } from '../context/LocaleContext';
 import MacroPulseBar from '../components/sgsyen/MacroPulseBar';
-import MacroEventDigestPanel from '../components/sgsyen/MacroEventDigestPanel';
-import QuantComparisonPanel from '../components/sgsyen/QuantComparisonPanel';
 
 const PAGE_SIZE = 8;
 
@@ -515,10 +513,6 @@ export default function ResearchPage() {
             </div>
           </div>
         </section>
-
-        <MacroEventDigestPanel />
-
-        <QuantComparisonPanel />
 
         <WeeklyEventFrame latestArticle={articles[0]} isZh={isZh} />
 
